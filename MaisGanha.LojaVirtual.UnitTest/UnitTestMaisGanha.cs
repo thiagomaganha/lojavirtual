199d183c18e55f276ca28c4fe02af4186502a59d
thiagomaganha <t.soares9@gmail.com>
2015-03-04 20:04:25 -0300

    Initial Commit

---
+++ MaisGanha.LojaVirtual.UnitTest/UnitTestMaisGanha.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MaisGanha.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestMaisGanha
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ MaisGanha.LojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MaisGanha.LojaVirtual.Web.MvcApplication" Language="C#" %>
