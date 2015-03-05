8192b5304a66bada8afafa2f5042ea8bb8b82dfa
thiagomaganha <t.soares9@gmail.com>
2015-03-04 22:57:34 -0300

    Repositorio e DBContext

---
+++ MaisGanha.LojaVirtual.Dominio/Repositorio/EFDBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaisGanha.LojaVirtual.Dominio.Entidades;
+
+namespace MaisGanha.LojaVirtual.Dominio.Repositorio
+{
+    public class EFDBContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingEntitySetNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produto");
+        }
+    }
+}
---
+++ MaisGanha.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using MaisGanha.LojaVirtual.Dominio.Entidades;
+
+namespace MaisGanha.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+        protected readonly EFDBContext _context = new EFDBContext();
+
+        public IEnumerable<Produto> Produtos
+        {
+            get { return _context.Produtos; }
+        }
+
+ 
+    }
+}
